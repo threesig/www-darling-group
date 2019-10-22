a30f91cfc64f91c2510fa5a56502dbc88acc09fb
David Lawson <david@threesig.com>
2019-10-21 23:03:25 -0600

    scroll and resize listening are operational

--- frontend/src/components/layout/Page.js
+++ frontend/src/components/layout/Page.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Block from './Block';
 import Header from './Header';
 import Footer from './Footer';
 import MainNavigation from './MainNavigation';
-import menus from '../../data/menus'
+import menus from '../../data/menus';
+import {getColorScheme} from '../../helpers';
 
 const Page = props => {
+  const refBlocks = useRef(null);
+  let blockPositions = null;
+
+
   const getBlockPositions = () => {
-    const blocks = document.querySelectorAll('.block');
+    const blocks = refBlocks.current.childNodes;
     const blockPositions = {};
     for (let i = 0; i < blocks.length; i++) {
       const block = blocks[i];
@@ -18,17 +23,65 @@ const Page = props => {
     }
     return blockPositions;
   }
+  const setBlockPositions = () => {
+    blockPositions = getBlockPositions();
+  }
+
+
+
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [bodyOffset, setBodyOffset] = useState(document.body.getBoundingClientRect());
+  const [scrollY, setScrollY] = useState(bodyOffset.top);
+  const [scrollDirection, setScrollDirection] = useState();
+
+  const scrollListener = (e) => {
+    setBodyOffset(document.body.getBoundingClientRect());
+    setScrollY(-bodyOffset.top);
+    setScrollDirection(lastScrollTop > -bodyOffset.top ? 'down' : 'up');
+    setLastScrollTop(-bodyOffset.top);
+  };
+  const resizeListener = (e) => {
+    setBlockPositions();    
+  }
+
+  // After Render, fire once;
   useEffect(() => {
-    console.log(getBlockPositions());
-  })
+    setBlockPositions();
+
+    window.addEventListener('scroll', scrollListener);
+    window.addEventListener('resize', resizeListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+      window.addEventListener('resize', resizeListener);
+    };
+  }, []);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   return (
-    <div id="page">
+    <div id="page" scrolly={scrollY}>
       <Header menuData={menus.main} />
       <div id="wrap">
         <MainNavigation menuData={menus.main} />
         <div id="main">
-          {props.children}
+          <div className="blocks" ref={refBlocks}>
+            {props.children}
+          </div>
         </div>
         <Footer menuData={menus} />
       </div>
