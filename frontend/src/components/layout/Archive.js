f13ae2c4550cb57e515b0667ce8de2e26e7f80d5
David Lawson <david@threesig.com>
2019-10-21 21:16:42 -0600

    wrap archive in page element

--- frontend/src/components/layout/Archive.js
+++ frontend/src/components/layout/Archive.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ArchiveItem from './ArchiveItem';
+import Page from './Page';
 const Archive = props => {
   const {pageKey, query} = props;
   const buildRepeaterKey = idx => `${pageKey}-${idx}`;
   const renderArchiveItem = (post, idx) => <li key={buildRepeaterKey(idx)} className="item-work" ><ArchiveItem post={post} /></li>
   return (
-    <>
+    <Page>
       <div className="block archive-heading color-scheme-light">
         <div className="interior">
           <h1 className="page-title">Our Work</h1>
@@ -18,7 +19,7 @@ const Archive = props => {
           </ul>
         </div>
       </div>
-    </>
+    </Page>
   );
 }
 export default Archive;
