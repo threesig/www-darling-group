015388374602123ea35d7ec23ceea5339debc719
David Lawson <david@threesig.com>
2019-10-22 00:15:31 -0600

    header color scheme detection is working

--- frontend/src/components/layout/Page.js
+++ frontend/src/components/layout/Page.js
@@ -4,57 +4,67 @@ import Header from './Header';
 import Footer from './Footer';
 import MainNavigation from './MainNavigation';
 import menus from '../../data/menus';
-import {getColorScheme} from '../../helpers';
 
 const Page = props => {
   const refBlocks = useRef(null);
-  let blockPositions = null;
-
-
   const getBlockPositions = () => {
     const blocks = refBlocks.current.childNodes;
     const blockPositions = {};
+    const colorSchemeRoot = 'color-scheme-';
+
     for (let i = 0; i < blocks.length; i++) {
       const block = blocks[i];
-      const colorSchemeContainer = [...block.classList].filter(myClass => myClass.startsWith('color-scheme'));
+      const colorSchemeContainer = [...block.classList].filter(myClass => myClass.startsWith(colorSchemeRoot));
       if (colorSchemeContainer.length) {
-        blockPositions[block.offsetTop] = colorSchemeContainer[0];
+        blockPositions[block.offsetTop] = colorSchemeContainer[0].substring(colorSchemeRoot.length);
       }
     }
     return blockPositions;
   }
-  const setBlockPositions = () => {
-    blockPositions = getBlockPositions();
-  }
 
-
-
+  
   const [lastScrollTop, setLastScrollTop] = useState(0);
   const [bodyOffset, setBodyOffset] = useState(document.body.getBoundingClientRect());
   const [scrollY, setScrollY] = useState(bodyOffset.top);
   const [scrollDirection, setScrollDirection] = useState();
+  const [headerColorScheme, setHeaderColorScheme] = useState();
+  const [blockPositions, setBlockPositions] = useState();
 
   const scrollListener = (e) => {
     setBodyOffset(document.body.getBoundingClientRect());
     setScrollY(-bodyOffset.top);
     setScrollDirection(lastScrollTop > -bodyOffset.top ? 'down' : 'up');
     setLastScrollTop(-bodyOffset.top);
+    setHeaderColorScheme(getHeaderColorScheme());
   };
   const resizeListener = (e) => {
-    setBlockPositions();    
+    setBlockPositions(getBlockPositions());    
   }
+
+  const getHeaderColorScheme = () => {
+    const wheat = Object.keys(blockPositions).filter(posVal => parseInt(posVal) <= scrollY);
+    return blockPositions[wheat.pop()]
+  }
+
 
   // After Render, fire once;
   useEffect(() => {
-    setBlockPositions();
+    setBlockPositions(getBlockPositions());
+  }, []);
 
+
+  useEffect(() => {
     window.addEventListener('scroll', scrollListener);
     window.addEventListener('resize', resizeListener);
     return () => {
       window.removeEventListener('scroll', scrollListener);
-      window.addEventListener('resize', resizeListener);
+      window.removeEventListener('resize', resizeListener);
     };
-  }, []);
+  });
+
+
+
+
 
 
 
@@ -74,8 +84,8 @@ const Page = props => {
 
   
   return (
-    <div id="page" scrolly={scrollY}>
-      <Header menuData={menus.main} />
+    <div id="page">
+      <Header menuData={menus.main} colorScheme={headerColorScheme} />
       <div id="wrap">
         <MainNavigation menuData={menus.main} />
         <div id="main">
