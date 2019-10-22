8a7442ec1778bf03fd43a10564547ec9f7a42db2
David Lawson <david@threesig.com>
2019-10-21 20:26:12 -0600

    factor heading content into blocks

--- frontend/src/components/layout/Pages/ProjectSingleF.js
+++ frontend/src/components/layout/Pages/ProjectSingleF.js
@@ -6,28 +6,26 @@ import { Link } from 'react-router-dom';
 import Image from '../../ui/Image';
 
 const ProjectSingleF = props => {
-  const buildBlockData = casestudy => {
-    return {
-      acf_fc_layout: 'showcase',
-      title: casestudy.post_title,
-      content: casestudy.showcaseContent,
-      image: casestudy.showcaseImage,
-      slug: casestudy.post_name
-    }
-  }
+
   const getBlock = (blockData, idx) => <Block key={getKey(idx)} data={{ ...blockData, blockIdx: idx }} />;
   const getKey = idx => `${props.pageKey}-block-${idx}`;
   const { blocks } = props.query[0];
   const { post_title, profileSubtitle, showcaseImage, post_name } = props.query[0];
+  const imageBlockData = {
+    acf_fc_layout: 'image',
+    image: showcaseImage,
+    type: 'hero'
+  }  
+  console.log(blocks);
   return (
     <PageF>
       <div className="block project-heading color-scheme-light">
         <div className="interior">
           <h1 className="page-title">{post_title}</h1>
           <h2 className="page-subtitle">{profileSubtitle}</h2>
-          <Image key={`${post_name}-featured-image`} data={showcaseImage} />
         </div>
       </div>
+      <Block data={imageBlockData} />
       {blocks.map(getBlock)}
       <Link to={`/projects/${props.next}`}>Next</Link>
     </PageF>
