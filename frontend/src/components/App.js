344cb3ae936cb721bc99ea69d9d05e97de11fbf1
David Lawson <david@threesig.com>
2019-10-21 20:25:39 -0600

    convert app to functional component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -21,17 +21,16 @@ import { groupBy } from 'lodash';
 import Loop from './layout/Loop';
 import ProjectSingleF from './layout/Pages/ProjectSingleF';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    const sorted_posts = props.posts.sort((a, b) => (a.menu_order > b.menu_order) ? 1 : -1);
-    this.posts = groupBy(sorted_posts, post => post.post_type);
-  }
-  getSingleQuery = (postType, slug) => this.posts[postType].filter(postType => postType.post_name === slug)
-  getNextSlug = (postType, slug) => {
+const App = props => {
+
+  const sorted_posts = props.posts.sort((a, b) => (a.menu_order > b.menu_order) ? 1 : -1);
+  const posts = groupBy(sorted_posts, post => post.post_type);
+
+  const getSingleQuery = (postType, slug) => posts[postType].filter(postType => postType.post_name === slug)
+  const getNextSlug = (postType, slug) => {
 
     // Grab the post type to search
-    const postGroup = this.posts[postType];
+    const postGroup = posts[postType];
 
     // Find the array index of the post that contains the slug
     const myIdx = postGroup.findIndex(post => post.post_name === slug)
@@ -44,27 +43,26 @@ export default class App extends React.Component {
 
     return nextSlug;
   }
-  render() {
-    this.getNextSlug('casestudy', 'stok-cold-brew')
-    return (
-      <div id="App">
-        <Router>
-          <Header menuData={menus.main} />
-          <div id="wrap">
-            <MainNavigation menuData={menus.main} />
-            <div id="page-area">
-              <Switch>
-                <Route exact path="/" render={props => <HomepageF {...props} pageKey={'home'} query={this.posts.page.filter(page => page.post_name === 'home')} featured={this.posts.casestudy.filter(casestudy => casestudy.isFeatured)} />} />
-                <Route path="/work" render={props => <Archive {...props} query={this.posts.casestudy} />} />
-                <Route path="/projects/:slug" render={props => <ProjectSingleF pageKey={props.match.params.slug} {...props} query={this.getSingleQuery('casestudy', props.match.params.slug)} next={this.getNextSlug('casestudy', props.match.params.slug)} />} />
-                <Route path="/:slug" render={props => <Loop {...props} query={this.posts.page.filter(page => page.post_name === props.match.params.slug)} />} />
-              </Switch>
-              <Footer menuData={menus} />
-            </div>
+  return (
+    <div id="App">
+      <Router>
+        <Header menuData={menus.main} />
+        <div id="wrap">
+          <MainNavigation menuData={menus.main} />
+          <div id="page-area">
+            <Switch>
+              <Route exact path="/" render={props => <HomepageF {...props} pageKey={'home'} query={posts.page.filter(page => page.post_name === 'home')} featured={posts.casestudy.filter(casestudy => casestudy.isFeatured)} />} />
+              <Route path="/work" render={props => <Archive {...props} query={posts.casestudy} />} />
+              <Route path="/projects/:slug" render={props => <ProjectSingleF pageKey={props.match.params.slug} {...props} query={getSingleQuery('casestudy', props.match.params.slug)} next={getNextSlug('casestudy', props.match.params.slug)} />} />
+              <Route path="/:slug" render={props => <Loop {...props} query={posts.page.filter(page => page.post_name === props.match.params.slug)} />} />
+            </Switch>
+            <Footer menuData={menus} />
           </div>
-        </Router>
-      </div>
+        </div>
+      </Router>
+    </div>
 
-    );
-  }
+  );
 }
+
+export default App;
