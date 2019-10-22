82bf528670d3a90f73c66a4355bb6ddddf04641d
David Lawson <david@threesig.com>
2019-10-21 20:36:24 -0600

    trim out old class-based components

--- frontend/src/components/layout/Pages/ServicesPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Page from '../Page';
-import Contact from '../Contact';
-
-export default class ServicesPage extends Page {
-  hookAfterContentBlocks = () => {
-    return <Contact />;
-  };
-}
