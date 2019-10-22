cffcbd62cab4129e8d6bd273d07cbd881f16a069
David Lawson <david@threesig.com>
2019-10-21 21:40:54 -0600

    convert footer to functional component

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Logo from '../ui/Logo';
 import Menu from './Menu';
-export default class Footer extends React.Component {
-  render() {
-    console.log(this.props);
-    return (
-      <footer id="footer">
-        <div className="interior">
-          <div className="cell">
-            <Logo />
-            <h3 className="tagline">Darling, be different.</h3>
-          </div>
-          <div className="cell">
-            <Menu items={this.props.menuData.main} />
-            <p className="copyright">&copy; 2019 Darling Group LLC</p>
-          </div>
+const Footer = props => {
+  const {menuData} = props;
+  return (
+    <footer key="site-footer" id="footer">
+      <div className="interior">
+        <div className="cell">
+          <Logo />
+          <h3 className="tagline">Darling, be different.</h3>
         </div>
-      </footer>
-    );
-  }
+        <div className="cell">
+          <Menu items={menuData.main} />
+          <p className="copyright">&copy; 2019 Darling Group LLC</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
+
+export default Footer;
