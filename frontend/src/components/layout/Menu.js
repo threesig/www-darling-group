c0d7646c85184fbffeae17275a8b261f3290719c
David Lawson <david@threesig.com>
2019-10-22 23:37:27 -0600

    rearchitect menus
    make a little more robust.  items are defined by key:val rather than
    just val

--- frontend/src/components/layout/Menu.js
+++ frontend/src/components/layout/Menu.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { slugify } from '../../helpers';
 import { Link } from "react-router-dom";
-export default class Menu extends React.Component {
-  static defaultProperties = {
-    items: []
+const Menu = props => {
+  
+  const {items} = props || {};
+
+  const getLinkTemplate = (itemText, itemDestination) => {
+    return itemDestination.includes(':')? <a href={itemDestination}>{itemText}</a>: <Link to={itemDestination}>{itemText}</Link>;
   }
-  getMenuItem(itemText) {
+  const getMenuItem = (itemText, itemDestination) => {
     const itemSlug = slugify(itemText);
-    return <li key={itemSlug} className="menu-item"><Link key={itemSlug} to={`/${itemSlug}`}>{itemText}</Link></li>;
+    return <li key={itemSlug} className="menu-item">{getLinkTemplate(itemText, itemDestination)}</li>;
   }
-  render() {
-    return (
-      <ul className="menu">
-        {this.props.items.map(this.getMenuItem)}
-      </ul>
-    );
-  }
+  return (
+    <ul className="menu">
+      {Object.keys(items).map(itemKey => getMenuItem(itemKey, items[itemKey]))}
+    </ul>
+  );
 }
+export default Menu;
--- frontend/src/data/menus.js
+++ frontend/src/data/menus.js
@@ -1,4 +1,4 @@
 export default {
-  main: ['Services', 'Work', 'Contact'],
-  social: ['Facebook', 'Twitter', 'Instagram']
+  main: {'Services':'services', 'Work':'work', 'Contact':'mailto:hello@darlingbedifferent.com'},
+  social: {'Facebook':'facebook', 'Twitter':'twitter', 'Instagram':'instagram'}
 }
