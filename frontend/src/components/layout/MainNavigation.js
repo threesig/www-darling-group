3aff7f458f4360ed766be630703a5e038c1c85b2
David Lawson <david@threesig.com>
2019-10-23 23:00:55 -0600

    explicitly add dark color scheme to main nav

--- frontend/src/components/layout/MainNavigation.js
+++ frontend/src/components/layout/MainNavigation.js
@@ -3,7 +3,7 @@ import Menu from './Menu';
 export default class MainNavigation extends React.Component {
   render() {
     return (
-      <nav id="nav-main" className="nav">
+      <nav id="nav-main" className="nav color-scheme-dark">
         <Menu items={this.props.menuData} />
       </nav>
     );
