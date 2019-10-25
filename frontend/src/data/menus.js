b6218202ab9b7ba5d1db48fe0fcd5b0d790d89cc
David Lawson <david@threesig.com>
2019-10-24 22:45:49 -0600

    fix urls in menu data

--- frontend/src/data/menus.js
+++ frontend/src/data/menus.js
@@ -1,4 +1,4 @@
 export default {
-  main: {'Services':'services', 'Work':'work', 'Contact':'mailto:hello@darlingbedifferent.com'},
+  main: {'Services':'/services', 'Work':'/work', 'Contact':'mailto:hello@darlingbedifferent.com'},
   social: {'Facebook':'facebook', 'Twitter':'twitter', 'Instagram':'instagram'}
 }
