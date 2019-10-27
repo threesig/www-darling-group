09e4aa5b248fc65ed335127c4a9ae684f1506bff
David Lawson <david@threesig.com>
2019-10-27 16:55:32 -0600

    point data source to production data

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,3 @@
 export default {
-  // contentDataUri: 'https://darling-website.s3-us-west-1.amazonaws.com/data/staging-data.json'
-  contentDataUri: '/data/staging-data.json'
+  contentDataUri: '/data/production-data.json'
 }
