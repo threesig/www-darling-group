37b3061f5b656612d534ba5704e13665ba87716d
David Lawson <david@threesig.com>
2019-10-27 14:43:44 -0600

    kill page scrollY

--- frontend/src/components/layout/Page.js
+++ frontend/src/components/layout/Page.js
@@ -97,7 +97,7 @@ const Page = props => {
       <Header menuData={menus.main} colorScheme={headerColorScheme} handleMainNavToggle={handleMainNavToggle} />
       <div id="wrap">
         <MainNavigation menuData={menus.main} />
-        <div id="main" ref={refMain} scrolly={scrollY}>
+        <div id="main" ref={refMain}>
           <div className="blocks" ref={refBlocks}>
             {props.children}
           </div>
