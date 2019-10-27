4ffe2061e79dd2da1b50e2f9320bb8230f95eff1
David Lawson <david@threesig.com>
2019-10-27 16:51:09 -0600

    archive styles

--- frontend/src/components/layout/ArchiveItem.js
+++ frontend/src/components/layout/ArchiveItem.js
@@ -2,22 +2,28 @@ import React from 'react';
 import Image from '../ui/Image';
 import { Link } from 'react-router-dom';
 
-export default class ArchiveItem extends React.Component {
-  getClassNames = () => {
+const ArchiveItem = props => {
+  const { ID, post_title, subtitle, showcaseImage, secondaryImage, post_name, post_type } = props.post;
+  
+  console.log(props.post);
+  const getClassNames = () => {
     return [
       'ArchiveItem',
       'post',
-      `type-${this.props.post.post_type}`
-    ].join();
+      `type-${post_type}`
+    ].join(' ');
   }
-  render() {
-    const { id, post_title, subtitle, showcaseImage, secondaryImage, post_name } = this.props.post;
-    return (
-      <article id={`post-${id}`} className={this.getClassNames()}>
-        <Image data={secondaryImage || showcaseImage} scaleOnHover={true} />
+
+
+  return (
+    <article id={`post-${ID}`} className={getClassNames()}>
+      <Image data={secondaryImage || showcaseImage} scaleOnHover={true} />
+      <div className="content-area">
         <h2 className="title"><Link to={`/projects/${post_name}`}>{post_title}</Link></h2>
-        <p className="services">{subtitle}</p>
-      </article>
-    );
-  }
+        <p className="content">{subtitle}</p>
+      </div>
+    </article>
+  );
 }
+
+export default ArchiveItem;
