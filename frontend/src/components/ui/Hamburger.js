e557b8278b54dba899d3da15d3a97fd55df65045
David Lawson <david@threesig.com>
2019-10-27 10:52:03 -0600

    contextual label for menu button

--- frontend/src/components/ui/Hamburger.js
+++ frontend/src/components/ui/Hamburger.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 const Hamburger = props => {
   const {handleMainNavToggle} = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = e => {
+    e.preventDefault();
+    handleMainNavToggle(e);
+    setIsOpen(!isOpen);
+  }
+
+  const readiedAction = isOpen ? 'Close' : 'Open';
+
   return (
-    <button className="hamburger" title="Menu Toggle" onClick={handleMainNavToggle}>
+    <button className="hamburger" title={`${readiedAction} Menu`} onClick={handleClick}>
       <div className="interior">
         <span className="bar"></span>
         <span className="bar"></span>
