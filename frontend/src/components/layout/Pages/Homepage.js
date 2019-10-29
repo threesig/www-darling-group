6f17a98e8e0f0b16bf2a56842d40c84c3eeaedcf
David Lawson <david@threesig.com>
2019-10-29 15:21:08 -0600

    migrate to block prepping function

--- frontend/src/components/layout/Pages/Homepage.js
+++ frontend/src/components/layout/Pages/Homepage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Block from '../Block';
 import Page from '../Page';
-import Contact from '../Contact';
+// import Contact from '../Contact';
 import FullBlocks from '../FullBlocks';
+import {compileBlocks} from '../../../helpers';
 
 const Homepage = props => {
   const buildBlockData = casestudy => {
@@ -17,16 +18,16 @@ const Homepage = props => {
     }
   }
   const renderShowcases = (casestudy, idx) => <Block key={getKey(idx)} data={{ ...buildBlockData(casestudy), blockIdx: idx }} />
-  const getBlock = (blockData, idx) => <Block key={getKey(idx)} data={{ ...blockData, blockIdx: idx }} />;
+  const getBlock = (blockData, idx) => <Block key={getKey(idx)} blockIndex={idx}  data={{ ...blockData, blockIdx: idx }} />;
   const getKey = idx => `${props.pageKey}-block-${idx}`;
   const { blocks } = props.query[0];
+  
+  const allBlocks = compileBlocks(blocks, props.featured.map(buildBlockData), { acf_fc_layout: 'contact' });
   
   return (
     <Page>
       <FullBlocks>
-        {blocks.map(getBlock)}
-        {props.featured.map(renderShowcases)}
-        <Contact />
+        {allBlocks.map(getBlock)}
       </FullBlocks>
     </Page>
   )
