d961a2866496846317e37c83697c407395493804
David Lawson <dlawson@lrxd.com>
2019-11-02 13:14:44 -0600

    correct block advance functionality
    pull blocks from ref, not from react children.  This gives us access to the dom element, not the react node.  allows us to more easily manipulate the classes on each element.

--- frontend/src/components/layout/FullBlocks.js
+++ frontend/src/components/layout/FullBlocks.js
@@ -7,47 +7,72 @@ const FullBlocks = props => {
 
   const normalizeBlocks = () => props.children.map(childBlock => childBlock.constructor === Array ? childBlock : [childBlock]);
   const getBlockCount = () => props.children ? normalizeBlocks().reduce((blockCount, blockContainer) => blockCount + blockContainer.length, 0) : 0;
+
   const blockCount = getBlockCount();
+  let blockIndex = blockCount > 0 ? 1 : 0;
+  setFullBlockCount(blockCount);
+  setFullBlockIndex(blockIndex);
+
+
 
   const activeClass = 'active';
   const advanceBlock = (direction) => {
     // Integer.  +1 Forward or -1 Backward.
     direction = direction || 1;
 
-    const { children } = props;
+    const { children } = refFullBlocks.current;
     if (children) {
-      const activeOrInactiveChildren = children.filter(block => {
-        let { classNames } = block.props.data;
-        classNames = classNames || '';
-
+      const activeOrInactiveBlocks = [...children].filter(block => {
+        const blockClassList = [...block.classList];
         return direction > 0  // Scrolling Forward
-          ? !classNames.split(' ').includes(activeClass) // find classes that are NOT active. Return the FIRST one to make it active.
-          : classNames.split(' ').includes(activeClass); // Find classes that ARE active.  Return the LAST one to make it inactive.
-
-        // ~~~TODO: GOING IN REVERSE, WE NEVER REMOVE `Active` FROM THE FIRST BLOCK
+          ? !blockClassList.includes(activeClass) // find classes that are NOT active. Return the FIRST one to make it active.
+          : blockClassList.includes(activeClass); // Find classes that ARE active.  Return the LAST one to make it inactive.
       });
 
+      let localBlockCount = fullBlockCount;
       switch (direction > 0) {
         case true: // Moving Forward
-          const nextBlock = activeOrInactiveChildren[0] // Return First Inactive block, so it can be made Active
+          /** activeOrInactiveBlocks === Inactive **/
+
+          // Next Block is the first Inactive Block
+          const nextBlock = activeOrInactiveBlocks[0]
+
+          // Activate Next Block
+          nextBlock.classList.add(activeClass);
+
+          // Recalculate Block Index
+          localBlockCount++;
 
           break;
         default:  // Moving Backward
           // First block always stays Active.
-          if (activeOrInactiveChildren.length > 1) {
-            const prevBlock = activeOrInactiveChildren.pop();
+          if (activeOrInactiveBlocks.length > 1) {
+
+            /** activeOrInactiveBlocks === Active **/
+
+            // Previous Block is the last Active Block
+            const prevBlock = activeOrInactiveBlocks.pop();
+
+            // Deactivate Previous Block
+            prevBlock.classList.remove(activeClass);
+
+            // Recalculate Block Inded
+            localBlockCount--;
           }
       }
+      setFullBlockCount(localBlockCount);
     }
   }
 
 
   useEffect(() => {
-    console.log('backward!', advanceBlock(-1));
+    // advanceBlock(1);
+    // advanceBlock(1);
+    // advanceBlock(1);
+    // advanceBlock(-1);
+    // advanceBlock(-1);
   }, []);
 
-  setFullBlockCount(blockCount);
-  setFullBlockIndex(blockCount > 0 ? 1 : 0);
   return (
     <div id="FullBlocks" ref={refFullBlocks} data-block-count={fullBlockCount} data-block-index={fullBlockIndex}>
       {props.children}
