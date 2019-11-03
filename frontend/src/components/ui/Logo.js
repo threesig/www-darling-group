ae8fef5ba88e87b9573217526334adbeb6dd8f11
David Lawson <dlawson@lrxd.com>
2019-11-03 10:40:14 -0700

    update logo  remove test style.

--- frontend/src/components/ui/Logo.js
+++ frontend/src/components/ui/Logo.js
@@ -1,14 +1,12 @@
 import React from 'react';
-export default class Logo extends React.Component {
-  static defaultProps = {
-    color: '#f00',
-    hw: 50
-  };
-  render() {
-    return (
-      <svg className="Logo" height={this.props.hw} width={this.props.hw} viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 20C0 8.954 8.954 0 20 0s20 8.954 20 20-8.954 20-20 20S0 31.046 0 20zm13 9h6.826C25.08 29 29 25.916 29 20.488 29 15.059 25.079 12 19.8 12H13v17z" fillRule="evenodd" />
-      </svg>
-    );
-  }
+const Logo = props => {
+  let { hw } = props;
+  hw = hw || 50;
+
+  // Color applied as `fill` on `path`
+  return (
+    <svg className="Logo" height={hw} width={hw} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 244 244"><g data-name="Layer 2"><path d="M122 0a122 122 0 10122 122A122 122 0 00122 0zm-1.23 163.43H88.6v-79h32.05c24.87 0 43.35 14.21 43.35 39.44s-18.48 39.56-43.23 39.56z" data-name="Layer 1" /></g></svg>
+  );
 }
+
+export default Logo;
