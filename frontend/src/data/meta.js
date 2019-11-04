f9eee7c0d4e48cbfea0811bdd3544de5d367277e
David Lawson <dlawson@lrxd.com>
2019-11-03 17:57:07 -0700

    add data blob that contains seo data

---
+++ frontend/src/data/meta.js
@@ -0,0 +1,67 @@
+const urlRoot = 'https://localhost:3000';
+
+export default {
+  "/": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+  "/services": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+  "/work": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+  "/projects/jopwell": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+  "/projects/the-north-face": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+  "/projects/curacubby": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+  "/projects/corepower-yoga": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+  "/projects/cooper-union": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+  "/projects/stok-cold-brew": {
+    "title": "Darling Group - A Denver-based Creative Agency",
+    "description": "We’re a design and development agency, specializing in mobile and web applications.",
+    "ogTitle": "Darling Design and Digital",
+    "ogDescription": "We design and develop custom mobile applications and digital platforms.",
+    "ogImage": "blah",
+  },
+}
