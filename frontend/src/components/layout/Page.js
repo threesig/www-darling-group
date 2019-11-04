83c50de9a095628e4a9d4db3fc4bd9ed30c4af66
David Lawson <dlawson@lrxd.com>
2019-11-03 18:00:12 -0700

    cleanup

--- frontend/src/components/layout/Page.js
+++ frontend/src/components/layout/Page.js
@@ -1,5 +1,4 @@
 import React, { useState, useLayoutEffect, useRef, useContext } from 'react';
-import Block from './Block';
 import Header from './Header';
 import Footer from './Footer';
 import MainNavigation from './MainNavigation';
