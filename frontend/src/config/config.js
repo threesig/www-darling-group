3a9415096e0d0b84831dfa406a92580c357ff50b
David Lawson <david@threesig.com>
2020-01-12 20:58:41 -0700

    change data url source to full url

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
 export default {
-  contentDataUri: '/data/production-data.json',
+  contentDataUri: 'http://darling-website.s3-website-us-west-1.amazonaws.com/data/production-data.json',
   pageTransitionTime: 750
 }
